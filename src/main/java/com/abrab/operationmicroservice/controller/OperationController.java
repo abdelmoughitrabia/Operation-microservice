93731b2a812fb59aee5307a525d15a5eea8c606c
abdelmoughitrabia <abdelmoughit.rabia@usmba.ac.ma>
2018-02-28 11:49:01 +0000

    Externalisation de la config vers un service de configuration

--- src/main/java/com/abrab/operationmicroservice/controller/OperationController.java
+++ src/main/java/com/abrab/operationmicroservice/controller/OperationController.java
@@ -3,6 +3,7 @@ package com.abrab.operationmicroservice.controller;
 import com.abrab.operationmicroservice.domain.Operation;
 import com.abrab.operationmicroservice.service.OperationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -16,6 +17,13 @@ public class OperationController {
 
     @Autowired
     private OperationService operationService;
+    @Value("${global}")
+    private String testConf;
+
+    @GetMapping("/conf")
+    public String testConf() {
+        return testConf;
+    }
 
     @GetMapping("/operations")
     public List<Operation> getOperations() {
