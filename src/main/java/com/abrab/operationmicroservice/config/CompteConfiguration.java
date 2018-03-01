7e711e5f7129dbb1a6439a399987cc0abdb0bbdb
abdelmoughitrabia <abdelmoughit.rabia@usmba.ac.ma>
2018-03-01 17:48:07 +0000

    Externalisation de la config vers un service de configuration

--- src/main/java/com/abrab/operationmicroservice/config/CompteConfiguration.java
+++ src/main/java/com/abrab/operationmicroservice/config/CompteConfiguration.java
@@ -1,10 +1,7 @@
 package com.abrab.operationmicroservice.config;
 
 import com.netflix.client.config.IClientConfig;
-import com.netflix.loadbalancer.AvailabilityFilteringRule;
-import com.netflix.loadbalancer.IPing;
-import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.PingUrl;
+import com.netflix.loadbalancer.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 
@@ -20,7 +17,7 @@ public class CompteConfiguration {
 
     @Bean
     public IRule ribbonRule(IClientConfig config) {
-        return new AvailabilityFilteringRule();
+        return new RoundRobinRule();
     }
 
 }
